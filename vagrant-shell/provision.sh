c09ed6b8b0708b1717890574cfac2770bfb3ccec
Rich Lewis <rich.lewis@oracle.com>
2017-12-01 10:00:23 -0600

    Updating

--- vagrant-shell/provision.sh
+++ vagrant-shell/provision.sh
@@ -4,13 +4,13 @@
 echo "Entering Shell Provisoning"
 
 yum makecache fast
-yum install deltarpm -y
-yum install wget -y
+yum install deltarpm --assumeyes
+yum install wget --assumeyes
 
 /bin/mv /etc/yum.repos.d/public-yum-ol7.repo /etc/yum.repos.d/public-yum-ol7.repo.orig
 wget -q http://yum.oracle.com/public-yum-ol7.repo -P /etc/yum.repos.d
 
-yum install yum-utils -y
+yum install yum-utils --assumeyes
 yum-config-manager --enable ol7_addons
 yum-config-manager --enable ol7_software_collections
 yum-config-manager --enable ol7_developer
